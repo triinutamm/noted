7a340e664a0952d3f7f2d26c29cd3e753ae9be97
triinutamm <triinu.tamm@tptlive.ee>
2020-03-19 10:13:43 +0200

    first commit

---
+++ Noted/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace Noted
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var noteListView = FindViewById<ListView>(Resource.Id.listView);
+            var addNoteButton = FindViewById<Button>(Resource.Id.addNote_btn);
+            var editNoteButton = FindViewById<Button>(Resource.Id.editNote_btn);
+            var deleteNoteButton = FindViewById<Button>(Resource.Id.deleteNote_btn);
+
+            addNoteButton.Click += (s, e) =>
+            {
+
+                var intent = new Intent(this, typeof(AddNote));
+                StartActivity(intent);
+            };
+
+            editNoteButton.Click += (s, e) =>
+            {
+
+                var intent = new Intent(this, typeof(EditNote));
+                StartActivity(intent);
+            };
+
+            deleteNoteButton.Click += (s, e) =>
+            {
+
+            };
+        }
+
+    }
+}
---
+++ Noted/ViewNote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Noted
+{
+    [Activity(Label = "ViewNote")]
+    public class ViewNote : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.viewNoteLayout);
+        }
+    }
+}
